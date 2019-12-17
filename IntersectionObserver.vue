2f1268e666ec877b746eef709bc64cd82802d3a8
Luke Waldner <luke@funkhaus.us>
2019-12-16 16:15:12 -0800

    Fixed bugs

--- IntersectionObserver.vue
+++ IntersectionObserver.vue
@@ -4,22 +4,17 @@ let observer
 let settings = {
     value: {
         root: null,
-        rootMargin: '0px',
+        rootMargin: "0px",
         threshold: 1.0
     },
     modifiers: {
         once: false
     }
 }
-
 // Creates Observer
 const initObserver = () => {
-    return new IntersectionObserver(
-        onIntersection,
-        settings.value
-    )
+    return new IntersectionObserver(onIntersection, settings.value)
 }
-
 // This is the function that runs when an Intersection occurs.
 // NOTE Intersection happens when it goes out of view too!
 // SEE https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
@@ -27,37 +22,32 @@ const initObserver = () => {
 const onIntersection = (entries, observer) => {
     entries.forEach(entry => {
         const hasClass = entry.target.classList.contains("has-intersected")
-
         // If "once" and el already has class we have nothing to do
         if (settings.modifiers.once && hasClass) return
-
         // Fire event with IntersectionObserverEntry object
         const event = new CustomEvent("has-intersected", { detail: entry })
         entry.target.dispatchEvent(event)
-
         // Add a class for convenience
-        if(entry.isIntersecting) {
+        if (entry.isIntersecting) {
             entry.target.classList.add("has-intersected")
         } else {
-            entry.target.remove.add("has-intersected")
+            entry.target.classList.remove("has-intersected")
         }
     })
 }
-
 // The Vue directive config
 export default {
     bind(el, binding) {
         // Save new settings to settings var for convenience
         Object.assign(settings, binding)
-
         // Setup
-        initObserver()
+        observer = initObserver()
     },
     unbind(el) {
         observer.disconnect()
     },
     inserted(el) {
-        observer.observe(el);
+        observer.observe(el)
     }
 }
 </script>
